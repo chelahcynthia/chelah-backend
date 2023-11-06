4e3192813cfb99a16d6e7ba64f0ef2e2dd60a764
chelahcynthia <cchelangat989@gmail.com>
2023-11-06 16:11:16 +0300

    install rest client for api testing

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 
 const express = require('express')
 const morgan = require('morgan')
-const createError = require('http-error')
+const createError = require('http-errors')
 require('dotenv').config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
