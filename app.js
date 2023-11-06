4aef5418817bf74294a1a73ac1bd15f67215f3b2
chelahcynthia <cchelangat989@gmail.com>
2023-11-06 16:57:13 +0300

    Creates routes folder

--- app.js
+++ app.js
@@ -6,6 +6,30 @@ require('dotenv').config()
 
 const app = express()
 
+app.get('/', async (req, res, next) => {
+    res.send('Hello from express.')
+  })
+
+  app.use(async (req, res, next) => {
+    // Use next to execute the next middleware
+    next(createError.NotFound())
+  })
+  
+// error handling
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+      error: {
+        // If status is not there throw internal server error (500)
+        status: err.status || 500, 
+        message: err.message,
+      },
+    })
+  })
+
+
+
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
