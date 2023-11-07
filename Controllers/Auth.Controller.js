99a2484839f4282faba7206dfc9e6e78378ed617
chelahcynthia <cchelangat989@gmail.com>
2023-11-07 13:05:47 +0300

    controller cleanup

---
+++ Controllers/Auth.Controller.js
@@ -0,0 +1,41 @@
+const createError = require('http-errors')
+const User = require("../Models/User.model");
+const {authSchema} = require('../helpers/validation_schema')
+
+module.exports = {
+    register: async (req, res, next) => {
+      try {
+       const result = await authSchema.validateAsync(req.body)
+        const doesExist = await User.findOne({ email: result.email })
+        if (doesExist)
+          throw createError.Conflict(`${result.email} is already been registered`)
+  
+        const user = new User(result)
+        const savedUser = await user.save()
+        const accessToken = await signAccessToken(savedUser.id)
+        const refreshToken = await signRefreshToken(savedUser.id)
+  
+        res.send({ accessToken, refreshToken })
+      } catch (error) {
+        if (error.isJoi === true) error.status = 422
+        next(error)
+      }
+    },
+    login:  async (req, res, next) => { 
+        try { 
+            const result = await authSchema.validateAsync(req.body)
+           const user = await User.findOne({ email: result.email })
+           if (!user) throw createError.NotFound('User not registered')
+            const isMatch = await user.isValidPassword(result.password)
+            if (!isMatch)
+            throw createError.Unauthorized('Username/password not valid')
+    
+        } catch (error) { 
+            if (error.isJoi === true)
+            return next(createError.BadRequest('Invalid Username/Password'))
+            next(error)
+        }
+    },
+    
+
+}
